0d12cd0f8617a40190fd9d623c25a6d7a949e10c
smithvyne <smithnkereuwem2@gmail.com>
2020-05-12 01:53:11 +0300

    update all methods with correct return values

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,7 +2,7 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
     n = 0
-    while n < self.length do
+    while n < self.length
       yield(self[n]) if self.is_a?(Array)
       yield(keys[n], self[keys[n]]) if self.is_a?(Hash)
       yield(self[n]) if self.is_a?(Range)
@@ -14,7 +14,7 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
     n = 0
-    while n < self.length do
+    while n < self.length
       yield(self[n], self.index(self[n])) if self.is_a?(Array)
       yield(keys[n], self[keys[n]]) if self.is_a?(Hash)
       yield(self[n]) if self.is_a?(Range)
@@ -33,7 +33,7 @@ module Enumerable
       result = {}
       self.my_each { |e_key, e_value| result[e_key] = e_value if yield(e_key, e_value) }
     end
-    p result
+    result
   end
 
   def my_all?(block = false)
@@ -46,7 +46,7 @@ module Enumerable
       end
       a &&= c
     end
-    p a
+    a
   end
 
   def my_any?(block = false)
@@ -59,7 +59,7 @@ module Enumerable
       end
       a ||= c
     end
-    p a
+    a
   end
 
   def my_none?(block = false)
@@ -72,7 +72,7 @@ module Enumerable
       end
       a ||= c
     end
-    p !a
+    !a
   end
 
   def my_count(block = false)
@@ -95,7 +95,7 @@ module Enumerable
     self.my_each do |element|
       result << yield(element)
     end
-    p result
+    result
   end
 
   def my_inject(block = false)
@@ -104,10 +104,9 @@ module Enumerable
       self.my_each do |element|
         accumulator = yield(accumulator, element)
       end
-       
     elsif !block
       self.my_each { |element| element == self[0] ? accumulator = self[0] : accumulator = yield(accumulator, element) }
     end
-    p accumulator
+    accumulator
   end
 end
